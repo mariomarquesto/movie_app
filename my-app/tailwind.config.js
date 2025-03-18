5f697165454f7c93448caf487dc2e89c7febe00f
mariomarquesto <mariomarquesto@gmail.com>
2025-03-17 21:13:40 -0300

    corregido

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014", // Usa un nombre válido en lugar de un código hexadecimal como clave
+        accent: "#AB8BFF",
+      },
+    },
   },
   plugins: [],
-}
-
+};
